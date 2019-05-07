0d8b1ab91b69081fc9ecf34b2eecc5618a4758e8
Jose Blas Camacho Taboada <joseblas@gmail.com>
2019-05-07 14:57:49 +0200

    Hola

--- src/main/scala/AkkaHttpMicroservice.scala
+++ src/main/scala/AkkaHttpMicroservice.scala
@@ -28,7 +28,7 @@ trait Service extends Protocols {
   def config: Config
   val logger: LoggingAdapter
 
-  def hello =  "Hello world"
+  def helo =  "Helo world"
 
   lazy val ipApiConnectionFlow: Flow[HttpRequest, HttpResponse, Any] =
     Http().outgoingConnection(config.getString("services.ip-api.host"), config.getInt("services.ip-api.port"))
